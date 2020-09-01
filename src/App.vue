a27599b705a6b16a33b3c2470584534a7b7a109d
dulongfei <497664492@qq.com>
2020-09-02 02:13:36 +0800

    'commit'

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,14 @@
             }
         },
         created() {
-
+            document.addEventListener('touchstart', function(event) {
+                if (event.touches.length > 1) {
+                    event.preventDefault()
+                }
+            })
+            document.addEventListener('gesturestart', function(event) {
+                event.preventDefault()
+            })
         },
         mounted() {
             this.updateUserList()
